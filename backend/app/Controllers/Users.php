4c5f2c133aa12c098b1fa85374a2b710107dc4cb
hosheewashitape <167269660+hoshewasheetape1018@users.noreply.github.com>
2025-11-25 02:34:39 +0800

    feat(frontend) Add Users controller and roadmap view; update routes and footer/header components
    - Created Users controller with index and roadmap methods.
    - Added new route for the roadmap page.
    - Implemented footer component with social links and resource sections.
    - Developed header component with navigation and mobile menu functionality.
    - Introduced roadmap view with a timeline of project phases and features.

---
+++ backend/app/Controllers/Users.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Users extends BaseController
+{
+    public function index(): string
+    {
+
+        return view('user/');
+    }
+ 
+    public function roadmap(): string
+    {
+
+        return view('user/roadmap');
+    }
+
+}
+
+
+
+
+   // public function headers(): string
+    // {
+    //     return view('components/header/header');
+    // }
+
+    // public function textchange(): string
+    // {
+    //     return view('user/textchange');
+    // }
